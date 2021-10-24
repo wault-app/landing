d2d79266f7a24ddc0a525bcb046d289deb9664a4
Gál Péter <pepyta118@gmail.com>
2021-10-24 22:12:53 +0200

    style: hide navbar from top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import EncryptionBlock from "@components/EncryptionBlock/EncryptionBlock";
 import FeatureList from "@components/FeatureList/FeatureList";
 import MottoComponent from "@components/MottoComponent";
-import Navbar from "@components/Navbar";
 import { Container } from "@mui/material";
 import { Fragment } from "react";
 
 const HomePage = () => {
     return (
         <Fragment>
-            <Navbar />
-            <Container sx={{ mt: "64px"}} maxWidth={"sm"}>
+            <Container maxWidth={"sm"}>
                 <MottoComponent />
             </Container>
             <Container maxWidth={"md"}>
